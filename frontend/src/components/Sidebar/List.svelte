6fd6603957910a4ce5675c8ea70255e7e22760c6
Mateusz Picheta <m@picheta.dev>
2024-10-08 21:59:52 +0200

    Move List title to sidebar-users

--- frontend/src/components/Sidebar/List.svelte
+++ frontend/src/components/Sidebar/List.svelte
@@ -3,17 +3,9 @@
     import DragDropList, { VerticalDropZone, reorder } from "svelte-dnd-list";
     import { selection_store } from '../../stores/selectionStore.js';
 
-    export let title = "";
-    export let collapsible = false;
     export let draggable;
     export let showItems = true;
     export let items = [];
-
-    function toggleShow() {
-        if (collapsible) {
-            showItems = !showItems;
-        }
-    }
 
     function onDrop({ detail: { from, to } }) {
         if (!to || from === to) {
@@ -24,33 +16,11 @@
     }
 </script>
 
-{#if title}
-    {#if collapsible}
-        <button
-            class="w-full flex items-center align-middle text-center mt-0 pt-0 pb-1 pl-3 cursor-pointer"
-            on:click={toggleShow}
-        >
-            <span class="inline-block transition-transform duration-150 ease-in-out" class:rotate-0={showItems} class:-rotate-90={!showItems}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" ><path fill="white" fill-rule="evenodd" d="M2.97 5.47a.75.75 0 0 1 1.06 0L8 9.44l3.97-3.97a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 0 1 0-1.06" clip-rule="evenodd"/></svg>
-            </span>
-            <p class="text-lg inline-block ml-1">
-                {title}
-            </p>
-        </button>
-    {:else}
-        <div class="w-full flex items-center align-middle text-center mt-0 pt-0 pr-20 pb-1 pl-2">
-            <p class="text-lg inline-block ml-1">
-                {title}
-            </p>
-        </div>
-    {/if}
-{/if}
-
 <ul class="cursor-pointer">
     {#if showItems}
         <!-- todo, make this godforsaken itemsize calculate automatically -->
         <DragDropList
-            id={title}
+            id="id"
             type={VerticalDropZone}
             itemSize={34}
             itemCount={items.length}
--- frontend/src/components/Sidebar/Sidebar-Users.svelte
+++ frontend/src/components/Sidebar/Sidebar-Users.svelte
@@ -20,6 +20,10 @@
         })
     }
 
+    function toggleShow() {
+            showChats = !showChats;
+    }
+
     getChats();
 </script>
 
@@ -28,10 +32,20 @@
         items={chats}
     />
 {:else}
+    <button on:click={toggleShow} class="w-full flex items-center align-middle text-center mt-0 pt-0 pb-1 pl-3 cursor-pointer group">
+        <span class="inline-block transition-transform duration-150 ease-in-out" class:rotate-0={showChats} class:-rotate-90={!showChats}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" ><path fill="white" fill-rule="evenodd" d="M2.97 5.47a.75.75 0 0 1 1.06 0L8 9.44l3.97-3.97a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 0 1 0-1.06" clip-rule="evenodd"/></svg>
+        </span>
+        <p class="text-lg inline-block ml-1">
+            Friends
+        </p>
+        <span class="ml-auto w-5 transition-opacity duration-150 ease-in-out opacity-0 group-hover:opacity-100">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><g fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></g></svg>
+        </span>
+    </button>
+
     <List
-        title="Friends"
         items={chats}
-        collapsible={true}
         bind:showItems={showChats}
     />
 {/if}
