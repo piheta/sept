cdbbc89006e2651ced33d7d5f571dd259fe5f9df
Mateusz Picheta <m@picheta.dev>
2024-10-21 13:09:04 +0200

    Dont scanln in p2p message handler

--- backend/services/p2p-message-hander.go
+++ backend/services/p2p-message-hander.go
@@ -1,8 +1,6 @@
 package services
 
 import (
-	"fmt"
-
 	"github.com/pion/webrtc/v4"
 )
 
@@ -10,10 +8,6 @@ func MessagingHandler(d *webrtc.DataChannel) {
 	for {
 		// Send the message as text
 		var messageSend string
-		_, err := fmt.Scanln(&messageSend)
-		if err != nil {
-			panic(err)
-		}
 
 		if err := d.SendText(messageSend); err != nil {
 			panic(err)
