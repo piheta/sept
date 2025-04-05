5e9910925d16b48b97f0e0938f7dedcc7c718d98
MagnusVestvik <magnusvestvik@proton.me>
2025-04-05 20:19:34 +0200

    Change: format footers

--- frontend/src/components/Footer/EmoteBox.svelte
+++ frontend/src/components/Footer/EmoteBox.svelte
@@ -1,55 +1,80 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-    export let mb;
-    export let chat_id;
-    export let sendMessage;
-    export let toggleShow;
+  export let mb;
+  export let chat_id;
+  export let sendMessage;
+  export let toggleShow;
 
-    let searchTerm = '';
-    let emotes = [];
+  let searchTerm = "";
+  let emotes = [];
 
-    const emoteImports = import.meta.glob('../../assets/emotes/*.webp', { eager: true });
+  const emoteImports = import.meta.glob("../../assets/emotes/*.webp", {
+    eager: true,
+  });
 
-    onMount(() => {
-        emotes = Object.entries(emoteImports).map(([path, module]) => {
-            const name = path.split('/').pop().replace('.webp', '');
-            return { url: module.default, name };
-        });
+  onMount(() => {
+    emotes = Object.entries(emoteImports).map(([path, module]) => {
+      const name = path.split("/").pop().replace(".webp", "");
+      return { url: module.default, name };
     });
+  });
 
-    const sendEmote = (url) => {
+  const sendEmote = (url) => {
+    sendMessage(
+      `<img class="mt-1.5 mb-1 rounded-md max-h-10 max-w-15" src="${url}" alt="gif" />`,
+      chat_id,
+    );
+    toggleShow(false);
+  };
 
-        sendMessage(`<img class="mt-1.5 mb-1 rounded-md max-h-10 max-w-15" src="${url}" alt="gif" />`, chat_id);
-        toggleShow(false);
-    };
-
-    $: filteredEmotes = searchTerm
-        ? emotes.filter((emote) => emote.name.toLowerCase().includes(searchTerm.toLowerCase()))
-        : emotes;
+  $: filteredEmotes = searchTerm
+    ? emotes.filter((emote) =>
+        emote.name.toLowerCase().includes(searchTerm.toLowerCase()),
+      )
+    : emotes;
 </script>
 
-<div class="h-64 w-72 p-2 rounded-md shadow-lg absolute left-[40vw]" style="bottom:calc({mb}px + 1rem); background-color: rgba(17, 24, 39, 0.98);">
-    <div class="flex relative mb-2 h-8">
-        <svg class="absolute left-2 top-2 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="11" cy="11" r="8" /><path d="m21 21l-4.3-4.3"/></g></svg>
-        <input
-            style="background-color: rgba(17, 24, 39, 0.6);"
-            class="w-full placeholder-gray-400 rounded-md pl-8 focus:outline-none shadow-xl font-normal text-[0.9rem]"
-            placeholder="Search Emotes"
-            autocapitalize="off"
-            autocomplete="off"
-            bind:value={searchTerm}
-        />
-    </div>
+<div
+  class="h-64 w-72 p-2 rounded-md shadow-lg absolute left-[40vw]"
+  style="bottom:calc({mb}px + 1rem); background-color: rgba(17, 24, 39, 0.98);"
+>
+  <div class="flex relative mb-2 h-8">
+    <svg
+      class="absolute left-2 top-2 text-gray-400"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      ><g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        ><circle cx="11" cy="11" r="8" /><path d="m21 21l-4.3-4.3" /></g
+      ></svg
+    >
+    <input
+      style="background-color: rgba(17, 24, 39, 0.6);"
+      class="w-full placeholder-gray-400 rounded-md pl-8 focus:outline-none shadow-xl font-normal text-[0.9rem]"
+      placeholder="Search Emotes"
+      autocapitalize="off"
+      autocomplete="off"
+      bind:value={searchTerm}
+    />
+  </div>
 
-    <div class="overflow-y-auto w-full grid grid-cols-[repeat(auto-fill,minmax(1.5rem,1fr))] gap-1 no-scrollbar">
-        {#each filteredEmotes as emote}
-            <img
-                on:click={() => sendEmote(emote.url)}
-                class="w-6 h-6 hover:cursor-pointer hover:opacity-65"
-                src={emote.url}
-                alt={emote.name}
-            />
-        {/each}
-    </div>
+  <div
+    class="overflow-y-auto w-full grid grid-cols-[repeat(auto-fill,minmax(1.5rem,1fr))] gap-1 no-scrollbar"
+  >
+    {#each filteredEmotes as emote}
+      <img
+        on:click={() => sendEmote(emote.url)}
+        class="w-6 h-6 hover:cursor-pointer hover:opacity-65"
+        src={emote.url}
+        alt={emote.name}
+      />
+    {/each}
+  </div>
 </div>
--- frontend/src/components/Footer/GifBox.svelte
+++ frontend/src/components/Footer/GifBox.svelte
@@ -1,109 +1,131 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-    export let mb;
-    export let chat_id;
-    export let sendMessage;
-    export let toggleShow;
+  export let mb;
+  export let chat_id;
+  export let sendMessage;
+  export let toggleShow;
 
-    const tenor = "AIzaSyDdux0EuzTX4pBG4H-il_X4xbe3p2BPOgg"; // Tenor said it can be used client side ¯\_(ツ)_/¯
-    let searchTerm = "";
+  const tenor = "AIzaSyDdux0EuzTX4pBG4H-il_X4xbe3p2BPOgg"; // Tenor said it can be used client side ¯\_(ツ)_/¯
+  let searchTerm = "";
 
-    let topGifsLeft = [];
-    let topGifsRight = [];
+  let topGifsLeft = [];
+  let topGifsRight = [];
 
-    async function searchGif(searchTerm) {
-        const clientkey = "Sept";
-        const lmt = 8;
+  async function searchGif(searchTerm) {
+    const clientkey = "Sept";
+    const lmt = 8;
 
-        const searchUrl = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${tenor}&client_key=${clientkey}&limit=${lmt}`;
+    const searchUrl = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${tenor}&client_key=${clientkey}&limit=${lmt}`;
 
-        try {
-            const response = await fetch(searchUrl);
-            const data = await response.json();
+    try {
+      const response = await fetch(searchUrl);
+      const data = await response.json();
 
-            // Update the GIF URLs using the response data
-            let topGifs = data.results;
-            topGifsLeft = topGifs.filter((_, index) => index % 2 === 0); // Even indices
-            topGifsRight = topGifs.filter((_, index) => index % 2 !== 0); // Odd indices
-        } catch (error) {
-            console.error("Error fetching data from Tenor API:", error);
-        }
+      // Update the GIF URLs using the response data
+      let topGifs = data.results;
+      topGifsLeft = topGifs.filter((_, index) => index % 2 === 0); // Even indices
+      topGifsRight = topGifs.filter((_, index) => index % 2 !== 0); // Odd indices
+    } catch (error) {
+      console.error("Error fetching data from Tenor API:", error);
     }
+  }
 
-    async function getFeaturedGifs() {
-        const clientkey = "Sept";
-        const lmt = 8;
+  async function getFeaturedGifs() {
+    const clientkey = "Sept";
+    const lmt = 8;
 
-        const featuredUrl = `https://tenor.googleapis.com/v2/featured?key=${tenor}&client_key=${clientkey}&limit=${lmt}`;
+    const featuredUrl = `https://tenor.googleapis.com/v2/featured?key=${tenor}&client_key=${clientkey}&limit=${lmt}`;
 
-        try {
-            const response = await fetch(featuredUrl);
-            const data = await response.json();
+    try {
+      const response = await fetch(featuredUrl);
+      const data = await response.json();
 
-            let topGifs = data.results;
-            topGifsLeft = topGifs.filter((_, index) => index % 2 === 0); // Even indices
-            topGifsRight = topGifs.filter((_, index) => index % 2 !== 0); // Odd indices
-        } catch (error) {
-            console.error("Error fetching featured GIFs:", error);
-        }
+      let topGifs = data.results;
+      topGifsLeft = topGifs.filter((_, index) => index % 2 === 0); // Even indices
+      topGifsRight = topGifs.filter((_, index) => index % 2 !== 0); // Odd indices
+    } catch (error) {
+      console.error("Error fetching featured GIFs:", error);
     }
+  }
 
-    function sendGif(giflink) {
-        sendMessage(`<img class="mt-1.5 mb-1 rounded-md max-h-72 max-w-72 w-5/6" src="${giflink}" alt="gif" />`, chat_id);
-        toggleShow();
+  function sendGif(giflink) {
+    sendMessage(
+      `<img class="mt-1.5 mb-1 rounded-md max-h-72 max-w-72 w-5/6" src="${giflink}" alt="gif" />`,
+      chat_id,
+    );
+    toggleShow();
+  }
+
+  function handleInput(event) {
+    if (searchTerm.length > 2) {
+      searchGif(searchTerm);
     }
+  }
 
-    function handleInput(event) {
-        if (searchTerm.length > 2) {
-            searchGif(searchTerm);
-        }
-    }
-
-    onMount(() => {
-        getFeaturedGifs();
-    });
+  onMount(() => {
+    getFeaturedGifs();
+  });
 </script>
 
-<div class="h-64 w-72 p-2 rounded-md shadow-lg absolute left-[40vw]" style="bottom:calc({mb}px + 1rem); background-color: rgba(17, 24, 39, 0.98);">
-    <div class="flex relative mb-2 h-8">
-        <svg class="absolute left-2 top-2 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="11" cy="11" r="8" /><path d="m21 21l-4.3-4.3"/></g></svg>
-        <input
-            style="background-color: rgba(17, 24, 39, 0.6);"
-            class="w-full placeholder-gray-400 rounded-md pl-8 focus:outline-none shadow-xl font-normal text-[0.9rem]"
-            placeholder="Search Tenor"
-            autocapitalize="off"
-            autocomplete="off"
-            bind:value={searchTerm}
-            on:input={handleInput}
+<div
+  class="h-64 w-72 p-2 rounded-md shadow-lg absolute left-[40vw]"
+  style="bottom:calc({mb}px + 1rem); background-color: rgba(17, 24, 39, 0.98);"
+>
+  <div class="flex relative mb-2 h-8">
+    <svg
+      class="absolute left-2 top-2 text-gray-400"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      ><g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        ><circle cx="11" cy="11" r="8" /><path d="m21 21l-4.3-4.3" /></g
+      ></svg
+    >
+    <input
+      style="background-color: rgba(17, 24, 39, 0.6);"
+      class="w-full placeholder-gray-400 rounded-md pl-8 focus:outline-none shadow-xl font-normal text-[0.9rem]"
+      placeholder="Search Tenor"
+      autocapitalize="off"
+      autocomplete="off"
+      bind:value={searchTerm}
+      on:input={handleInput}
+    />
+  </div>
+
+  <div
+    class="overflow-y-auto w-full relative h-[calc(100%-2.5rem)] flex gap-2 no-scrollbar"
+  >
+    <div class="w-1/2">
+      {#each topGifsLeft as gif}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+        <img
+          on:click={() => sendGif(gif.media_formats?.gif?.url)}
+          class="mb-2 w-full rounded-md shadow-lg hover:cursor-pointer hover:opacity-65"
+          src={gif.media_formats?.nanogif?.url}
+          alt="Preview GIF"
         />
+      {/each}
     </div>
 
-    <div class="overflow-y-auto w-full relative h-[calc(100%-2.5rem)] flex gap-2 no-scrollbar">
-        <div class="w-1/2">
-            {#each topGifsLeft as gif}
-                <!-- svelte-ignore a11y-click-events-have-key-events -->
-                <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-                <img
-                    on:click={() => sendGif(gif.media_formats?.gif?.url)}
-                    class="mb-2 w-full rounded-md shadow-lg hover:cursor-pointer hover:opacity-65"
-                    src={gif.media_formats?.nanogif?.url}
-                    alt="Preview GIF"
-                />
-            {/each}
-        </div>
-
-        <div class="w-1/2">
-            {#each topGifsRight as gif}
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-            <img
-                on:click={() => sendGif(gif.media_formats?.gif?.url)}
-                class="mb-2 w-full rounded-md shadow-lg hover:cursor-pointer hover:opacity-65"
-                src={gif.media_formats?.nanogif?.url}
-                alt="Preview GIF"
-            />
-            {/each}
-        </div>
+    <div class="w-1/2">
+      {#each topGifsRight as gif}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+        <img
+          on:click={() => sendGif(gif.media_formats?.gif?.url)}
+          class="mb-2 w-full rounded-md shadow-lg hover:cursor-pointer hover:opacity-65"
+          src={gif.media_formats?.nanogif?.url}
+          alt="Preview GIF"
+        />
+      {/each}
     </div>
+  </div>
 </div>
